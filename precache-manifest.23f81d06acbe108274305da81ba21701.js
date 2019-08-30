af3f9b949aa1eaac79b2ca44e3a7659ee5830be4
megfan <megfa@yahoo.com>
2019-08-30 10:47:29 +0200

    Updates

---
+++ precache-manifest.23f81d06acbe108274305da81ba21701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47484a86073e9930b1dc005f150796be",
+    "url": "/DEV_M11_final/index.html"
+  },
+  {
+    "revision": "c550af3cfdaa7509d5e7",
+    "url": "/DEV_M11_final/static/css/main.a3b03f9c.chunk.css"
+  },
+  {
+    "revision": "d0cfad77418ace4b0e93",
+    "url": "/DEV_M11_final/static/js/2.3c7a9054.chunk.js"
+  },
+  {
+    "revision": "c550af3cfdaa7509d5e7",
+    "url": "/DEV_M11_final/static/js/main.786c88d5.chunk.js"
+  },
+  {
+    "revision": "a3c9a5a35145138eda68",
+    "url": "/DEV_M11_final/static/js/runtime~main.cde90316.js"
+  }
+]);
